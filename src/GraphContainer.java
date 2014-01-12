6055a04dda0af1841a53eaec0642981e7837de86
George Panayotov <georgep1245@gmail.com>
2014-01-11 23:03:21 -0800

    Construct the vertexcomponents. Note the layout code will probably have to move from the constructor

--- src/GraphContainer.java
+++ src/GraphContainer.java
@@ -1,13 +1,40 @@
 import javax.swing.JComponent;
+import java.util.Set;
+import java.util.ArrayList;
+import java.awt.Point;
 
 public class GraphContainer extends JComponent
 {
     private Digraph<String> graph;
+    private Set<String> vertices;
+    private ArrayList<VertexComponent> vertexComponents;
 
-    public GraphContainer(Digraph<String> graph)
+    public GraphContainer(Digraph<String> newGraph)
     {
-        this.graph = graph;
-        // TODO: create vertexComponents for each vertex
+        this.graph = newGraph;
+
+        this.vertices = this.graph.vertices();
+        this.vertexComponents = new ArrayList<VertexComponent>(this.vertices.size());
+
+        VertexComponent component = null;
+
+        this.setSize(500, 500);
+
+        int i = 0;
+        int size = this.vertices.size();
+        for(String vertex : this.vertices)
+        {
+            component = new VertexComponent(vertex);
+            this.vertexComponents.add(component);
+
+            double theta = (((double) i) * 2 * Math.PI) / ((double)size);
+            double x = 100 * Math.cos(theta) + 250;
+            double y = 100 * Math.sin(theta) + 250;
+
+            component.setLocation(new Point((int)x, (int)y));
+            i ++;
+        }
+
         // TODO: create arrow containers for each out-edge. Need another class for that.
         // TODO: helper method to draw them
     }
